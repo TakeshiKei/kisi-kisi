7b0816afad0521586e7ad67e2a36d7f343cc97a6
Kei <80631822+TakeshiKei@users.noreply.github.com>
2022-11-21 10:22:46 +0700

    Kisi kisi UAS Kevin

---
+++ strada_kisi-kisi/No 1.js
@@ -0,0 +1,7 @@
+const angka = [10, 25, 53, 5, 20];
+
+angka.push(60);
+
+angka.sort((a , b) => a - b);
+
+console.log(angka);
---
+++ strada_kisi-kisi/No 3.js
@@ -0,0 +1,6 @@
+const sebuahAngkaBulat = parseInt("30");
+const sebuahAngkaDesimal= parseFloat(64.6464.toFixed(2));
+
+hasil = sebuahAngkaBulat + sebuahAngkaDesimal;
+
+console.log(hasil);
